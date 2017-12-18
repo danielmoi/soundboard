a91464df09a73925b95a41f21d8b7212f47d7912
danielmoi <daniel@ampersandmoi.com>
2017-12-18 20:12:43 +1100

    Connect UI elements to VC

---
+++ Soundboard/SoundViewController.swift
@@ -0,0 +1,36 @@
+//
+//  SoundViewController.swift
+//  Soundboard
+//
+//  Created by Daniel Moi on 18/12/17.
+//  Copyright © 2017 Daniel Moi. All rights reserved.
+//
+
+import UIKit
+
+class SoundViewController: UIViewController {
+
+    @IBOutlet weak var recordButton: UIButton!
+    
+    
+    @IBOutlet weak var nameTextField: UITextField!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        print("OMG")
+        // Do any additional setup after loading the view.
+    }
+
+    @IBAction func recordTapped(_ sender: Any) {
+    }
+    
+    
+    @IBAction func playTapped(_ sender: Any) {
+    }
+    
+    @IBAction func saveTapped(_ sender: Any) {
+    }
+    
+    
+}
